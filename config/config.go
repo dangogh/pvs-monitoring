a48ae81d0c21d0423b16a8e0d88c3480999e2e4d
Dan Kirkwood <dangogh@gmail.com>
2026-05-25 14:10:55 -0700

    Add reconnect backoff fields to Config with Duration YAML type

--- config/config.go
+++ config/config.go
@@ -5,21 +5,40 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"gopkg.in/yaml.v3"
 )
+
+// Duration wraps time.Duration to support YAML unmarshaling from strings like "30s".
+type Duration time.Duration
+
+func (d Duration) Duration() time.Duration { return time.Duration(d) }
+
+func (d *Duration) UnmarshalYAML(value *yaml.Node) error {
+	dur, err := time.ParseDuration(value.Value)
+	if err != nil {
+		return fmt.Errorf("invalid duration %q: %w", value.Value, err)
+	}
+	*d = Duration(dur)
+	return nil
+}
 
 const defaultAddr = "ws://192.168.191.155:9002"
 
 // Config holds all runtime configuration for pvs-monitor.
 type Config struct {
-	Addr string `yaml:"addr"`
+	Addr                     string   `yaml:"addr"`
+	ReconnectInitialInterval Duration `yaml:"reconnect_initial_interval"`
+	ReconnectMaxInterval     Duration `yaml:"reconnect_max_interval"`
 }
 
 // Default returns a Config populated with built-in defaults.
 func Default() Config {
 	return Config{
-		Addr: defaultAddr,
+		Addr:                     defaultAddr,
+		ReconnectInitialInterval: Duration(time.Second),
+		ReconnectMaxInterval:     Duration(30 * time.Second),
 	}
 }
 
